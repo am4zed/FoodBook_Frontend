f2b3f9051cd514b19f90e98edfad2a69d5f5acce
user <gibson.stephen92@gmail.com>
2019-06-02 14:51:13 +0100

    added missing test, skipping all tests

--- src/components/Searchbar/__test__/Searchbar.test.js
+++ src/components/Searchbar/__test__/Searchbar.test.js
@@ -11,47 +11,56 @@ describe("searchbar", () => {
     });
 
     xit("snapshot", () => {
-        // TODO
+        // TODO to be done last
     });
 
-    it("has state for current value of the input", () => {
+    xit("has state for current value of the input", () => {
         const wrapper = shallow(<Searchbar />);
         expect(wrapper.state().value).toBeDefined();
-    })
+    });
 
-    it("has onSubmit prop", () => {
+    xit("has onSubmit prop", () => {
         const wrapper = shallow(<Searchbar onSubmit={props.onSubmit} />);
         const instance = wrapper.instance();
         expect(instance.props.onSubmit).toBeDefined()
-    })
+    });
 
-    it("has a form element", () => {
+    xit("has a form element", () => {
         const wrapper = shallow(<Searchbar />);
         expect(wrapper.exists("form")).toBeTruthy();
-    })
+    });
 
-    it("has an input", () => {
+    xit("has an input", () => {
         const wrapper = shallow(<Searchbar />);
         expect(wrapper.exists("input")).toBeTruthy();
-    })
+    });
 
-    it("calls the onChange when the input changes", () => {
+    xit("calls the onChange when the input changes", () => {
         const evt = { target: { name: "searchbar", value: "t" } }
         const wrapper = shallow(<Searchbar />);
         const instance = wrapper.instance();
         const spy = sinon.spy(instance, "onChange");
         wrapper.find("input").simulate('change', evt)
         expect(spy.calledOnce).toBeTruthy();
-    })
+    });
 
-    it("updates the state with the value in the input", () => {
+    xit("updates the state with the value in the input", () => {
         const evt = { target: { name: "searchbar", value: "t" } }
         const wrapper = shallow(<Searchbar />);
         wrapper.find("input").simulate('change', evt)
         expect(wrapper.state().value).toEqual("t")
-    })
+    });
 
-    it("calls onSubmit when submitted", () => {
+    xit("calls onSubmit function when submitted", () => {
+        const { onSubmit } = props;
+        const wrapper = shallow(<Searchbar onSubmit={onSubmit} />);
+        const instance = wrapper.instance();
+        const spy = sinon.spy(instance, "onSubmit")
+        wrapper.find("form").simulate("submit");
+        expect(spy.calledOnce).toBeTruthy();
+    });
+
+    xit("calls onSubmit prop when submitted", () => {
         const { onSubmit } = props;
         const wrapper = shallow(<Searchbar onSubmit={onSubmit} />);
         const instance = wrapper.instance();
