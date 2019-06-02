9de3a97a51164ebde47f9b16715ffa80d37170ee
user <gibson.stephen92@gmail.com>
2019-06-02 13:23:23 +0100

    snapshot test added. added missing enyzme to json serializer

--- src/components/ProtectedRoute/__test__/ProtectedRoute.test.js
+++ src/components/ProtectedRoute/__test__/ProtectedRoute.test.js
@@ -9,18 +9,52 @@ const TestComponent = (props) => {
 const props = {
     path: "/",
     component: TestComponent,
+    renderComponent: () => <TestComponent />,
     validatingSession: true
 }
 
-
-
-void describe("ProtectedRoute", () => (
+void describe("ProtectedRoute", () => {
     void it("renders without crashing", () => {
         const wrapper = mount(
             <Router>
-                <ProtectedRoute component={props.component} />
+                <ProtectedRoute path={props.path} component={props.component} />
             </Router>
         );
-        return
     })
-))
+
+    void it("snapshot", () => {
+        const wrapper = mount(
+            <Router>
+                <ProtectedRoute path={props.path} component={props.component} validatingSession={false} />
+            </Router>
+        );
+        expect(wrapper).toMatchSnapshot();
+    })
+
+    void it("renders the component when passed in directly", () => {
+        const wrapper = mount(
+            <Router>
+                <ProtectedRoute path={props.path} component={props.component} />
+            </Router>
+        );
+        expect(wrapper.find("div").exists()).toBeTruthy();
+    })
+
+    void it("renders the component when passed in via function", () => {
+        const wrapper = mount(
+            <Router>
+                <ProtectedRoute path={props.path} render={props.renderComponent} />
+            </Router>
+        );
+        expect(wrapper.find("div").exists()).toBeTruthy();
+    })
+
+    void it("does not render the passed in component if it's validating", () => {
+        const wrapper = mount(
+            <Router>
+                <ProtectedRoute path={props.path} component={props.component} validatingSession={props.validatingSession} />
+            </Router>
+        );
+        expect(wrapper.find("div").exists()).toBeFalsy();
+    })
+})
