90ec9c0784f9b1bea5fb394a1ad76ec9d4c5c9f9
Mark <nirvanafoo86@hotmail.com>
2019-06-03 13:11:02 +0100

    Initial Recipe commit

--- src/components/Recipe/Recipe.js
+++ src/components/Recipe/Recipe.js
@@ -1,5 +1,22 @@
+import React from 'react';
+import './recipe.css';
+import Ingredient from '../Ingredient';
+import Method from '../Method';
 
+const Recipe = (props) => {
 
+  return (
+    <>
+      <h1>{props.recipeName}</h1>
+      <h2>Ingredients :</h2>
+      <Ingredient />
+      <h2>Methods :</h2>
+      <Method />
+    </>
+  );
+
+};
+export default Recipe;
 
 // <img - image of the dish for the recipe/>;//Float left
 // <div>Ingredients</div>;//Float right
