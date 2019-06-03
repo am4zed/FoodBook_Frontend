8e2c4bd6632c5f67f801e04c2a8a6ba143249883
Mark <nirvanafoo86@hotmail.com>
2019-06-03 13:12:01 +0100

    removed comments detailing layout of component

--- src/components/Recipe/Recipe.js
+++ src/components/Recipe/Recipe.js
@@ -17,8 +17,3 @@ const Recipe = (props) => {
 
 };
 export default Recipe;
-
-// <img - image of the dish for the recipe/>;//Float left
-// <div>Ingredients</div>;//Float right
-// <div>Methods</div>;// Positioned underneath ingredients
-// <div>Reviews</div>;// Positioned bottom of the page
