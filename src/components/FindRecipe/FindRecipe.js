87020876c2a89e3626cee9ab7aa556ee8eea4628
Amy Edwardson <am4zed11@gmail.com>
2019-06-04 11:21:30 +0100

    Added tests for FindRecipe

--- src/components/FindRecipe/FindRecipe.js
+++ src/components/FindRecipe/FindRecipe.js
@@ -11,3 +11,5 @@ const FindRecipe = () => {
     </>
   )
 }
+
+export default FindRecipe;
--- src/components/FindRecipe/__test__/FindRecipe.test.js
+++ src/components/FindRecipe/__test__/FindRecipe.test.js
@@ -1,5 +1,20 @@
+import React from "react";
+import SearchBar from "../../Searchbar/Searchbar.js"
+import FindRecipe from "../FindRecipe.js"
+import { shallow } from 'enzyme';
+
 describe("FindRecipe", () => {
-    it("renders without crashing", () => {
-        // TODO
+
+    let wrapper;
+
+    beforeEach(() => wrapper = shallow(<FindRecipe />));
+    // TESTS FAILING
+    it("renders a <Searchbar>", () => {
+        expect(wrapper.containsMatchingElement(<Searchbar></Searchbar>).toEqual(true));
     })
+
+    it("renders without crashing", () => {
+        expect(wrapper).toMatchSnapshot();
+    })
+    
 })
