202f98fc2a8f77d7a41d70fb93218a4477ff4975
Mark <nirvanafoo86@hotmail.com>
2019-06-04 17:17:05 +0100

    aligning content in the centre

--- src/components/Discover/Discover.js
+++ src/components/Discover/Discover.js
@@ -1,23 +1,4 @@
 
-import React from 'react';
-import Searchbar from "../Searchbar/Searchbar"
-import { withRouter } from "react-router-dom"
-
-const Discover = (props) => {
-
-    const recipes = props.recipes.map(() => {
-        return <div>This is a recipe card</div>
-    })
-
-    return (
-        <div>
-            <Searchbar onSubmit={props.onSubmit} />
-            <div className="this-will-be-a-flex-box">
-                {/* {recipes} */}
-            </div>
-        </div>
-    )
-=======
 import React from "react";
 import Searchbar from "../Searchbar/Searchbar";
 import { withRouter } from "react-router-dom";
@@ -30,7 +11,6 @@ const Discover = props => {
       <DiscoveryGrid hits={props.hits} onClick={props.onClick} />
     </div>
   );
->>>>>>> 218faeca375045dc1eb3c3f94026b5a27f6588fd
 };
 
 export default withRouter(Discover);
