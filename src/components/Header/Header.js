dea85fef4a470aa4db25e93a086f85e45a54d0d4
Amy Edwardson <am4zed11@gmail.com>
2019-06-04 15:19:44 +0100

    styling header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Logo from '../Logo/Logo.js';
+import "./header.css";
 
 const Header = () => {
   return (
-    <>
-      <Logo />
-      <button className="my_recipes">My Recipes</button>
-    </>
+    <div className="header">
+      < Logo className="logo" />
+      <div><button className="my_recipes">My Recipes</button>
+      </div>
+    </div>
   )};
 
 export default Header;
